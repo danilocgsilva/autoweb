2c42eeb5b12d5a84db86a1a6ac6bacd8e2fcc088
Danilo Carlos de Góes Silva <danilocgsilva@gmail.com>
2014-07-07 23:20:59 -0300

    working on zipping code

--- download.php
+++ download.php
@@ -8,14 +8,22 @@ if (empty($name)) {
   $name = 'index';
 }
 
+// Provides the final name of the file
+$final_name = $name . "." . $format;
+
 // Content, loads from a string in the program
 $file_content = file_get_contents('empty_webpage/index.txt');
 
-$file_content_compressed = gzcompress($file_content
+// Saves the file before zipping
+$fp = fopen('files/tmp/' . $final_name, 'w');
+fwrite($fp, $file_content);
+fclose($fp);
 
-$final_name = $name . "." . $format;
-
-print $file_content;
+// Zip file
+$zip = new ZipArchive;
+$zip->open('files/tmp/' . $final_name . ".zip", ZipArchive::CREATE);
+$zip->addFile('files/tmp/' . $final_name);
+$zip->close();
 
 header("Content-type: text/plain; charset=utf-8");
 header("Content-disposition: attachment; filename=\"" . $final_name . "\"");
