62b0ff8a4225ce54f47ed845b87c8cc6ab5e09ce
Danilo Silva <danilocgsilva@gmail.com>
2014-07-08 13:25:18 -0300

    stylesheet added

--- index.php
+++ index.php
@@ -3,23 +3,29 @@ require_once('vars.php');
 ?>
 <!DOCTYPE xml:lang="pt-br" lang="pt-br" html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
-	<head>
-		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <title><?php print $name ?></title>
-	</head>
+    <link rel="stylesheet" type="text/css" href="style.css" />
+  </head>
 
   <body>
     <div id="wrapper">
       <h1><?php echo $name ?></h1>
       <div id="body">
-        <form name="download" method="GET" action="download.php">
+
+        <select name="selecttask" form="form_download" size="12">
+          <option value="emptyhtml">Basic HTML</option>
+          <option value="wp_plugin">WordPress plugin</option>
+        </select>
+
+        <form id="form_download" name="download" method="GET" action="download.php">
           <input type="radio" name="format" value="html" checked>html
           <input type="radio" name="format" value="php">php<br />
           <input type="text" name="name" />
           <input type="submit" value="Cria" />
         </form>
-        <div id="code">
-        </div>
+
       </div> <!-- body's end -->
     </div>
 
