ebad51b8795b1fee08b8b924ada3b857468beeb1
Danilo Carlos de Góes Silva <danilocgsilva@gmail.com>
2014-12-08 16:47:24 -0200

    Changing the structure

--- download.php
+++ download.php
@@ -1,23 +1,8 @@
 <?php
 
+// Common infos from all processing page
 $name = $_GET['name'];
-$format = $_GET['format'];
 $chunk = $_GET['chunk'];
 
-// Provides default value for empty name's form
-if (empty($name)) {
-  $name = 'index';
-}
-
-// Provides the final name of the file
-$final_name = $name . "." . $format;
-
-// Content, loads from a string in the program
-$file_content = file_get_contents($chunk . '/index.txt');
-
-error_log($file_content);
-
-header("Content-type: text/plain; charset=utf-8");
-header("Content-disposition: attachment; filename=\"" . $final_name);
-
-echo $file_content;
+// Load the right file
+require_once($chunk . '/action.php');
