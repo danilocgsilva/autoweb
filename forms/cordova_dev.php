7043ca93b0470bd2f4e9fa1b491cb2b0bb9067f0
Danilo Silva <danilocgsilva@gmail.com>
2016-05-26 19:36:22 -0300

    Working in fillling textarea from cordova_dev

--- forms/cordova_dev.php
+++ forms/cordova_dev.php
@@ -2,7 +2,7 @@
     <?php if ($page_source == 'cordova_dev') echo 'style="display: inline-block"'; ?> id="cordova_dev" class="switchingForm" name="cordova_dev">
 
     <?php print_input("buttonName", "text", "Button Name", '<br /><br />', true, ""); ?>
-    <input id="add_cordova_button" onClick="alert(toId('Agora sim tenho o que'));" type="button" value="Add Button" />
+    <input id="add_cordova_button" type="button" value="Add Button" />
 
   <textarea id="cordova_textarea"></textarea>
   </form>
--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function toId(name) {
   var nameParts = name.split(" ");
   var idNamed = "";
-  
+
   // To follow javascript convesion, the first character mus be lowercase
   nameParts[0] = nameParts[0].charAt(0).toLowerCase() + nameParts[0].slice(1);
-  
-  
+
+
   for (var i = 1; i < nameParts.length; i++) {
     nameParts[i] = nameParts[i].charAt(0).toUpperCase() + nameParts[i].slice(1);
   }
@@ -21,48 +21,57 @@ function toVar(name) {
 
 window.onload = function () {
 
+  // Functions where elements must be already created
     function switchScreen(tela) {
-
         for (var i = 0; i < telas.length; i++) {
             if (telas[i] == tela) {
-                //document.getElementById(telas[i]).style.display = 'inline-block';
                 document.getElementById(telas[i]).className = "inlineblock";
             } else {
-                //document.getElementById(telas[i]).style.display = 'none';
                 document.getElementById(telas[i]).className = "switchingForm";
             }
         }
     }
 
-    function toCordovaCode(buttonName) {
-      var areaText = "";
 
-      areaText = "<input type=\"button\" value=\"" + buttonName + "\" />";
 
-      return areaText;
-    }
 
+    // Main index elements
     var selectPanel = document.getElementById("selectPanel");
+    var forms_holder = document.querySelector(".inner-content.form_holder");
+
+
+    // Form wordpress_plugin elements
     var wordpress_plugin_description_element = document.getElementById("plugin-php");
-    var wordpress_plugin_description_initial_value = wordpress_plugin_description_element.value;
     var pluginNameEl = document.getElementById("wordpress_plugin_name");
     var pluginDescriptionEl = document.getElementById("wordpress_plugin_description");
     var pluginLicenseEl = document.getElementById("wordpress_plugin_license");
     var pluginAuthorNameEl = document.getElementById("wordpress_plugin_author_name");
     var pluginAuthorUriEl = document.getElementById("wordpress_plugin_author_uri");
-    var forms_holder = document.querySelector(".inner-content.form_holder");
+
+
+    // Form cordova_dev elements
+    var inpButtonName = document.getElementById("buttonName");
+    var btnAddCordovaButton = document.getElementById("add_cordova_button");
+    var atxtCordovaTextarea = document.getElementById("cordova_textarea");
+
+
+    // Values initialization
+    var wordpress_plugin_description_initial_value = wordpress_plugin_description_element.value;
     var form_children = forms_holder.children;
     var telas = [];
+
     for (var i = 0; i < form_children.length; i++) {
       telas.push(form_children[i].id);
     }
 
-    updatePHPfile1(wordpress_plugin_description_initial_value);
 
-    selectPanel.onchange = function () {
-        switchScreen(this.value);
+    // Constructs
+    updatePHPfile1(wordpress_plugin_description_initial_value);
+    selectPanel.onchange = function() {
+      switchScreen(this.value);
     }
 
+    // Methods insite wordpress plugin form
     pluginNameEl.onkeyup = function () {
         updatePHPfile1(wordpress_plugin_description_initial_value);
     };
@@ -79,6 +88,16 @@ window.onload = function () {
         updatePHPfile1(wordpress_plugin_description_initial_value);
     };
 
+
+    // Methods insite cordova_dev form
+    btnAddCordovaButton.onclick = function() {
+      toCordovaCode();
+    }
+
+    function toCordovaCode() {
+      areaText = "<input type=\"button\" id=\"" + toId(inpButtonName.value) + "\" name=\"" + toId(inpButtonName.value) + "\" value=\"" + inpButtonName.value + "\" />";
+      atxtCordovaTextarea.value = areaText;
+    }
 }
 
 function updatePHPfile1(wordpress_plugin_description_initial_value) {
@@ -88,7 +107,6 @@ function updatePHPfile1(wordpress_plugin_description_initial_value) {
     var pluginlicense = document.getElementById("wordpress_plugin_license").value;
     var pluginauthor = document.getElementById("wordpress_plugin_author_name").value;
     var pluginauthoruri = document.getElementById("wordpress_plugin_author_uri").value;
-
     var pluginTextAreaElement = document.getElementById("plugin-php");
     var newText = wordpress_plugin_description_initial_value
         .replace('%PLUGINNAME%', pluginname)
